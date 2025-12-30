1795a1c35019cba982f225a306ea5239698ccead
redjon.varfi <redjon.varfi@advantagesolutions.net>
2025-12-30 09:25:47 +0100

    test the port to run the app

--- src/metrics/metrics.module.ts
+++ src/metrics/metrics.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MetricsController } from './metrics.controller';
 import { MetricsService } from './metrics.service';
 
+
 @Module({
   controllers: [MetricsController],
   providers: [MetricsService],
